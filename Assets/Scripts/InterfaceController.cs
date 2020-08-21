a0ddc8afb4fb4ab3da465e6cc395706622b3d12b
Jose Vives Iznardo <acachitoro@gmail.com>
2020-08-21 16:34:52 +0200

    Adding base UI interface

---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour
+{
+
+    public Text scoreText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(float newScoreToAdd)
+    {
+
+    }
+
+}
